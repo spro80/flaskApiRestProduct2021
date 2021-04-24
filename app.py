1a12ab5f7dc1b69caad928345f7730abbb07ee7a
David D <dsdiazs@falabella.cl>
2021-04-23 21:24:23 -0400

    add path list

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Healtcheck Get in /"
+
+
+@app.route('/product/list')
+def products_list():
+    print('Init in products_list')
+    return 'Return all products !!!'
