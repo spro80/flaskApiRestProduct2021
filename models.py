c18a6fc8576afd519fad32661501b516fa2c980e
David D <dsdiazs@falabella.cl>
2021-05-16 17:35:34 -0400

    add Model Users

--- models.py
+++ models.py
@@ -17,3 +17,17 @@ class Product(db.Model):
             f'brand: {self.brand, }'
             f'stock: {self.stock}'
         )
+
+
+class Users(db.Model):
+    #id_user = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_user = db.Column(db.Integer, primary_key=True)
+    name_user = db.Column(db.String(50))
+    pass_user = db.Column(db.String(50))
+
+    def __str__(self):
+        return(
+            f'Id: {self.id_user, }'
+            f'sku: {self.name_user, }'
+            f'name: {self.pass_user, }'
+        )
